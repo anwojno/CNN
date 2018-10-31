97d6e416888674a8442aba0727a2a54e9ff42ffd
anwojno <an.wojno@gmail.com>
2018-10-31 12:51:45 +0100

    oo

--- CNN.py
+++ CNN.py
@@ -1,5 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 10:12:55 2018
 
-print ("test"))
+@author: Anna
+"""
+
+import theano
+import tensorflow
+import keras
+
+print ("test")
 print("test2")
 print("neurony")
 print("lul")
+
