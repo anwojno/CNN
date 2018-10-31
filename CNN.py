7b630bfc64249b741ef3d129c0a6063f64073847
anwojno <an.wojno@gmail.com>
2018-10-31 23:32:10 +0100

    finished product

--- CNN.py
+++ CNN.py
@@ -12,13 +12,18 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense  # fully connected layer
-from keras.preprocessing.image import ImageDataGenerator  # for preprocessing images  
+from keras.preprocessing.image import ImageDataGenerator  # for preprocessing training and test images   
+from skimage.io import imread  # for loading image for prediction
+from skimage.transform import resize
+import numpy as np  # for preprocessing image used for prediction
+
+
 
 """ initializing CNN """
 classifier = Sequential()
 
 
-""" convolution layer """
+""" first convolution layer """
 classifier.add(Convolution2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))  
 """ Args:
     number of filters: can be 32, 64, 128 ... 
@@ -28,7 +33,16 @@ classifier.add(Convolution2D(32, (3, 3), input_shape = (64, 64, 3), activation =
 """
 
 
-""" pooling layer """
+""" first pooling layer """
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+
+""" second convolution layer """
+classifier.add(Convolution2D(32, (3, 3), activation = 'relu'))  
+# don't need to include input_shape since we work on pooled feature maps
+
+
+""" second pooling layer """
 classifier.add(MaxPooling2D(pool_size=(2,2)))
 
 
@@ -95,9 +109,29 @@ test_set = test_datagen.flow_from_directory(
             class_mode='binary')
 
 # fitting CNN model on our training set and testing it's performance on our test set
-# conda install pillow in your virtual env or pip install pillow if you're not using virtual env
+# conda install pillow in your env or pip install pillow if you're not using env
 classifier.fit_generator(training_set,
-                         steps_per_epoch=8000, # number of images in epoch
+                         steps_per_epoch=(8000/32), #  number of samples of your dataset divided by the batch size
                          epochs=25,
                          validation_data=test_set,
-                         validation_steps=2000)  # number of images in test set
+                         validation_steps=(2000/32))  # number of samples of your validation dataset divided by the batch size
+
+
+""" making new prediction """
+
+class_labels = {v: k for k, v in training_set.class_indices.items()} # creates a dictionary with mapping, ex. 0 = cat, 1 = dog
+ 
+test_image = imread('dataset/single_prediction/cat_or_dog_2.jpg')
+test_image = resize(test_image, (64,64))
+test_image = np.expand_dims(test_image, axis=0) 
+""" Adding another dimension to image, because predict function uses 4 dimensions - last one for batch number 
+    Args:
+     image: the image we're predicting
+     axis: the position of the index of the dimension we're adding
+"""
+if (np.max(test_image) > 1):
+    test_image = test_image/255.0  # preprocessing the image so that the pixels have value between 0 and 1
+ 
+prediction = classifier.predict_classes(test_image) # result is an array with one value, ex 1 for dog, 0 for cat
+ 
+print(class_labels[prediction[0][0]])
